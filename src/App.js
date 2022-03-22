aff9c4d1314d4a109a09a04623cc7637b0ebb73b
Tohid Shabanloo <tshabanloo@Hotmail.com>
2022-03-22 22:48:08 +0430

    2

--- src/App.js
+++ src/App.js
@@ -4,12 +4,13 @@ import "./index.css";
 import Header from "./components/Header/Header";
 import MyLayout from "./components/MyLayout/MyLayout";
 import "antd/dist/antd.css";
-
+import MyCarousel from "./components/MyCarousel/MyCarousel";
 
 function App() {
   return (
     <div className="App">
       <MyLayout />
+      <MyCarousel />
     </div>
   );
 }
---
+++ src/components/MyCarousel/MyCarousel.js
@@ -0,0 +1,25 @@
+import { Carousel } from "antd";
+import "./MyCarousel.css";
+
+const MyCarousel = (contentStyle) => {
+  return (
+    <div className="main">
+      <Carousel autoplay>
+        <div>
+          <h3 style={contentStyle}>1</h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>2</h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>3</h3>
+        </div>
+        <div>
+          <h3 style={contentStyle}>4</h3>
+        </div>
+      </Carousel>
+    </div>
+  );
+};
+
+export default MyCarousel;
