97f6a9dd76d853078e52d2a4487a273a6a228a3d
Tohid Shabanloo <tshabanloo@Hotmail.com>
2022-03-22 17:43:30 +0430

    install ant d

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <button className="button"> Home</button>
+      <button className="button">Biography</button>
+      <button className="button">Photos</button>
+      <button className="button">Contact Us</button>
+    </div>
+  );
+};
+export default Header;
