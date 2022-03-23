a0e4347c0c045ccaff45725214790ee356fb00cc
Tohid Shabanloo <tshabanloo@Hotmail.com>
2022-03-23 12:14:47 +0430

    add posts

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import Header from "./components/Header/Header";
 import MyLayout from "./components/MyLayout/MyLayout";
 import "antd/dist/antd.css";
 import MyCarousel from "./components/MyCarousel/MyCarousel";
+import Posts from "./components/Posts/Posts";
 
 function App() {
   return (
     <div className="App">
       <MyLayout />
+
       <MyCarousel />
     </div>
   );
---
+++ src/components/Posts/Posts.js
@@ -0,0 +1,64 @@
+import "./Posts.css";
+
+const Posts = () => {
+  return (
+    <div className="posts-container">
+      <span className="posts">
+        <img
+          className="img"
+          src="https://www.tohidsh.com/_next/image?url=%2Fuploads%2F2018%2F09%2FArbat-bg.jpg&w=750&q=75"
+        ></img>
+        <h4 className="post-title">
+          داستان گیتار زدن در خیابان آربات؛ سی روز در روسیه با 300 دلار
+        </h4>
+      </span>
+      <span className="posts">
+        <img
+          className="img"
+          src="https://www.tohidsh.com/_next/image?url=%2Fuploads%2Fflorence%2Fflorence-bg.jpg&w=750&q=75"
+        ></img>
+        <h4 className="post-title">
+          فلورانس؛ موزه ای بدون سقف؛ لوکا و مهمان نوازی ایتالیایی
+        </h4>
+      </span>
+      <span className="posts">
+        <img
+          className="img"
+          src="https://www.tohidsh.com/_next/image?url=%2Fuploads%2F2019%2F06%2Ffindingfarideh.jpg&w=750&q=75"
+        ></img>
+        <h4 className="post-title">
+          در جستجوی فریده؛ از فیلم تا واقعیت و ملاقات فریده{" "}
+        </h4>
+      </span>
+      <span className="posts">
+        <img
+          className="img"
+          src="https://www.tohidsh.com/_next/image?url=%2Fuploads%2Fthaifood%2Fthaifood-bg.jpg&w=750&q=75"
+        ></img>
+        <h4 className="post-title">
+          فستیوال غذاهای تایلندی؛ طعم جدید و هیجان‌انگیز برای ذائقه های مختلف{" "}
+        </h4>
+      </span>
+      <span className="posts">
+        <img
+          className="img"
+          src="https://www.tohidsh.com/_next/image?url=%2Fuploads%2Fwhoisbackpacker%2Fwhoisbackpacker-bg2.jpg&w=750&q=75"
+        ></img>
+        <h4 className="post-title">
+          زندگی یک بک پکر؛ تعریف سفر برای هر کسی متفاوت است{" "}
+        </h4>
+      </span>
+      <span className="posts">
+        <img
+          className="img"
+          src="https://www.tohidsh.com/_next/image?url=%2Fuploads%2Frome%2Frome-bg.jpg&w=750&q=75"
+        ></img>
+        <h4 className="post-title">
+          سفرنامه رُم؛ موزه شش هزار ساله، دیدار با جیانکارلو لوکوئنزی{" "}
+        </h4>
+      </span>
+    </div>
+  );
+};
+
+export default Posts;
